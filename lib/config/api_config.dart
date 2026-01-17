3794512d9787b03d849d5c40d718a6e59d8e222c
Iasonas Zakynthinos <zak@Iasonass-MacBook-Pro.fritz.box>
2026-01-17 20:25:35 +0200

    frontend prep

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,12 +1,5 @@
 import 'dart:io';
 
 class ApiConfig {
-  /// If you're running Django on your Mac:
-  /// - iOS Simulator can use 127.0.0.1
-  /// - Android Emulator must use 10.0.2.2
-  /// - Physical phone must use your Mac’s LAN IP (e.g., 192.168.1.50)
-  static String get baseUrl {
-    if (Platform.isAndroid) return 'http://10.0.2.2:8000';
-    return 'http://127.0.0.1:8000';
-  }
+  static const String baseUrl = "https://brookie-qmcm.onrender.com";
 }
